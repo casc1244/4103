cd233fa00a90205f52813114fbb346b7e429e160
casc1244 <1356602646@qq.com>
2020-04-10 15:14:26 +0800

    4103.cpp

--- 4103/4103.cpp
+++ 4103/4103.cpp
@@ -1,20 +1,28 @@
-﻿// 4103.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+using namespace std;
 
-#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	Mat srcMat = imread("D:\\360downloads\\9.png");
+	Mat dstMat;
+
+	Canny(srcMat, dstMat, 50, 200, 3);
+	//canny话 边缘
+
+	vector<Vec4i> lines;
+
+	HoughLinesP(dstMat, lines, 1, CV_PI / 180, 50, 50, 10);
+
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		Vec4i l = lines[i];
+		line(srcMat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 1, LINE_AA);
+	}
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
